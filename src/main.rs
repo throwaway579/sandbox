877c35e09fa1563b3fda5d2daef28dcb7a173f49
Draphar <draphar-code@protonmail.com>
2019-09-11 18:43:30 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[cfg(feature = "error")]
 #[test]
 fn error(){
     panic!();
+}
+
+#[cfg(not(feature = "error"))]
+#[test]
+fn error(){
+    println!("ok");
 }
